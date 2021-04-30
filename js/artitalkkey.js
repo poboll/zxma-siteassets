bb1c74ed44f665a506b228838b360d726b0d6d80
zxmacc <zhup10588@gmail.com>
2021-05-01 03:17:47 +0800

    12

---
+++ js/artitalkkey.js
@@ -0,0 +1,15 @@
+new Artitalk({
+    appId: 'bWKbEWaOu1mrfqweSCSLnJ1C-MdYXbMMI',  //替换为你的appId
+    appKey: 'tUr1EPp0h5n9h7iy5y6EgU1X', //替换为你的appKey
+  	pageSize: 1,
+  	color1: "#49b1f5",
+  	color2: "#00c4b6",
+    serverURL: "https://bWKbEWaO.api.lncldglobal.com",
+    //Hg****Nq替换为你的appId前八位
+});
+$(".power").css("padding","10px");
+$(".power").css("text-align","left");
+$("#switchUser").css("padding-right","10px");
+$("#pubShuo").css("padding-right","10px");
+$("#pubComment").css("padding-right","10px");
+$("#uploadSource").css("padding-right","10px");
